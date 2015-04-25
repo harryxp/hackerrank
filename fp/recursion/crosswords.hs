1c8b2e7cb8b2c5680d3e90843cb3332723675496
Harry Pan <pan.xingzhi@gmail.com>
2015-04-24 19:05:01 -0700

    Finished crosswords.

--- fp/recursion/crosswords.hs
+++ fp/recursion/crosswords.hs
@@ -1,7 +1,8 @@
 import Control.Monad (replicateM)
 import Data.Array
-import Data.List (intercalate, intersect, permutations, transpose)
+import Data.List (intercalate, intersect, partition, permutations, transpose)
 import Data.List.Split (splitOn)
+import qualified Data.Map as Map
 import Text.Regex.Posix (MatchArray, Regex, matchAll, makeRegex)
 
 import Test.HUnit
@@ -11,24 +12,27 @@ import Test.HUnit
 dimension = 10
 
 main :: IO ()
-main = replicateM (dimension+1) getLine >>= (putStrLn . solveCrosswords)
+main = replicateM (dimension+1) getLine >>= putStrLn . showCrosswords . solveCrosswords . parseInput
 
 type Coordinates = (Int, Int)
+data Orientation = H | V
+  deriving (Eq, Show)
 
 data Blank = Blank
   { start       :: Coordinates
   , end         :: Coordinates
   , len         :: Int
-  , orientation :: Char                 -- 'h' or 'v'
+  , orientation :: Orientation
   } deriving (Eq, Show)
 
-solveCrosswords :: [String] -> String
-solveCrosswords lines = (printOutput . reallySolveCrosswords . parseInput) lines
+type FilledBlank = (Blank, String)
 
 tests = TestList
   [ TestLabel "parseInput" testParseInput
-  , TestLabel "reallySolveCrosswords" testReallySolveCrosswords
-  , TestLabel "printOutput" testPrintOutput
+  , TestLabel "solveCrosswords" testSolveCrosswords
+  , TestLabel "showCrosswords" testShowCrosswords
+  , TestLabel "showOneLine horizontal" testShowOneLineHorizontal
+  , TestLabel "showOneLine vertical" testShowOneLineVertical
   ]
 
 ---- parseInput
@@ -39,8 +43,8 @@ parseInput input =
       grid = take dimension input
       words :: [String]
       words = (splitOn ";" . head . drop dimension) input
-      hBlanks = (concat . map (constructBlanksFromList 'h') . getIndiceAndRows) grid
-      vBlanks = (concat . map (constructBlanksFromList 'v') . getIndiceAndCols) grid
+      hBlanks = (concat . map (constructBlanks H) . getIndiceAndRows) grid
+      vBlanks = (concat . map (constructBlanks V) . getIndiceAndCols) grid
   in
     (hBlanks ++ vBlanks, words)
 
@@ -50,29 +54,29 @@ getIndiceAndRows m = zip [1..dimension] m
 getIndiceAndCols :: [[a]] -> [(Int, [a])]
 getIndiceAndCols m = getIndiceAndRows (transpose m)
 
-constructBlanksFromList :: Char -> (Int, String) -> [Blank]
-constructBlanksFromList hOrV (index, v) =
+constructBlanks :: Orientation -> (Int, String) -> [Blank]
+constructBlanks o (index, line) =
   let segments :: [MatchArray]
-      segments = matchAll (makeRegex "--+" :: Regex) v -- at least two minuses
+      segments = matchAll (makeRegex "--+" :: Regex) line -- at least two minuses
   in
-    map (constructBlank hOrV index) segments
+    map (constructBlank o index) segments
 
-constructBlank :: Char -> Int -> MatchArray -> Blank
-constructBlank hOrV index segment =
+constructBlank :: Orientation -> Int -> MatchArray -> Blank
+constructBlank o index segment =
   let matchOffsetAndLength = segment ! 0
       matchOffset = fst matchOffsetAndLength + 1
       matchLength = snd matchOffsetAndLength
-      startCoord = if hOrV == 'h' then (index,matchOffset) else (matchOffset,index)
-      endCoord = if hOrV == 'h' then (index,matchOffset+matchLength-1) else (matchOffset+matchLength-1,index)
+      startCoord = if o == H then (index,matchOffset) else (matchOffset,index)
+      endCoord = if o == H then (index,matchOffset+matchLength-1) else (matchOffset+matchLength-1,index)
   in
-    Blank { start = startCoord , end = endCoord , len = matchLength , orientation = hOrV }
+    Blank { start = startCoord , end = endCoord , len = matchLength , orientation = o }
 
 testParseInput = TestCase (assertEqual "parseInput" expected (parseInput input))
   where
-    expected = ([ Blank {start=(4,2),end=(4,6),len=5,orientation='h'}
-                , Blank {start=(8,3),end=(8,8),len=6,orientation='h'}
-                , Blank {start=(1,2),end=(6,2),len=6,orientation='v'}
-                , Blank {start=(4,6),end=(10,6),len=7,orientation='v'}
+    expected = ([ Blank {start=(4,2),end=(4,6),len=5,orientation=H}
+                , Blank {start=(8,3),end=(8,8),len=6,orientation=H}
+                , Blank {start=(1,2),end=(6,2),len=6,orientation=V}
+                , Blank {start=(4,6),end=(10,6),len=7,orientation=V}
                 ],
                 ["LONDON","DELHI","ICELAND","ANKARA"])
     input = [ "+-++++++++"
@@ -88,45 +92,44 @@ testParseInput = TestCase (assertEqual "parseInput" expected (parseInput input))
             , "LONDON;DELHI;ICELAND;ANKARA"
             ]
 
----- reallySolveCrosswords
+---- solveCrosswords
 -- the idea is that we just simply pair up Blanks with words and filter out the illegal ones
-reallySolveCrosswords :: ([Blank], [String]) -> [(Blank, String)]
-reallySolveCrosswords (blanks, words) =
+solveCrosswords :: ([Blank], [String]) -> [FilledBlank]
+solveCrosswords (blanks, words) =
   let solutions = filter validateCrosswords (zipWithPermutation blanks words)
   in
     case solutions of
-      [] -> error "no solution found"
+      []        -> error "no solution found"
       otherwise -> head solutions
 
-validateCrosswords :: [(Blank, String)] -> Bool
+validateCrosswords :: [FilledBlank] -> Bool
 validateCrosswords xs = all validatePair (choose2 xs)
 
-validatePair ((b1@Blank {start=_,end=_,len=len1,orientation=o1}, s1), (b2@Blank {start=_,end=_,len=len2,orientation=o2}, s2)) =
-  len1 == length s1 && len2 == length s2 &&
-  (o1 == o2 || joinProperly b1 s1 b2 s2)
+validatePair :: (FilledBlank, FilledBlank) -> Bool
+validatePair ((b1, s1), (b2, s2)) =
+  len b1 == length s1 && len b2 == length s2 &&
+  (orientation b1 == orientation b2 || joinProperly (b1, s1) (b2, s2))
 
-joinProperly :: Blank -> String -> Blank -> String -> Bool
-joinProperly b1 s1 b2 s2 =
+joinProperly :: FilledBlank -> FilledBlank -> Bool
+joinProperly (b1, s1) (b2, s2) =
   let coords1 = getAllCoords b1
       coords2 = getAllCoords b2
       junction = intersect coords1 coords2
   in
-    null junction || getCharAtCoords b1 (head junction) s1 == getCharAtCoords b2 (head junction) s2
+    null junction || getCharAtCoords (b1, s1) (head junction) == getCharAtCoords (b2, s2) (head junction)
 
 -- get all the coordinates a Blank occupies
 getAllCoords :: Blank -> [Coordinates]
 getAllCoords b@Blank {start=(x,y),end=_,len=l,orientation=o} =
   case o of
-  'h' -> map (\n -> (x,y+n)) [0..l-1]
-  'v' -> map (\n -> (x+n,y)) [0..l-1]
-  otherwise -> error "this is just impossible"
+    H -> map (\n -> (x,y+n)) [0..l-1]
+    V -> map (\n -> (x+n,y)) [0..l-1]
 
-getCharAtCoords :: Blank -> Coordinates -> String -> Char
-getCharAtCoords b@Blank {start=(x,y),end=_,len=_,orientation=o} (jx,jy) word =
+getCharAtCoords :: FilledBlank -> Coordinates -> Char
+getCharAtCoords (b@Blank {start=(x,y),end=_,len=_,orientation=o}, word) (jx,jy) =
   case o of
-  'h' -> word !! (jy-y)
-  'v' -> word !! (jx-x)
-  otherwise -> error "this is just impossible"
+    H -> word !! (jy-y)
+    V -> word !! (jx-x)
 
 -- zipWithPermutation ['a','b'] [1,2] yields [[('a',1), ('b',2)], [('a',2), ('b',1)]]
 zipWithPermutation :: [a] -> [b] -> [[(a,b)]]
@@ -137,31 +140,76 @@ zipWithPermutation xs ys =
   then error "lists must be of the same length"
   else map (zip xs) (permutations ys)
 
+-- choose2 [1,2,3] yields [(1,2), (1,3), (2,3)]
 choose2 :: [a] -> [(a,a)]
 choose2 [] = []
 choose2 (x:[]) = []
 choose2 (x:xs) = map (\x' -> (x, x')) xs ++ choose2 xs
 
-testReallySolveCrosswords = TestCase (assertEqual "reallySolveCrosswords" expected (reallySolveCrosswords input))
+testSolveCrosswords = TestCase (assertEqual "solveCrosswords" expected (solveCrosswords input))
   where
-    expected = [ (Blank {start=(4,2),end=(4,6),len=5,orientation='h'}, "DELHI")
-               , (Blank {start=(8,3),end=(8,8),len=6,orientation='h'}, "ANKARA")
-               , (Blank {start=(1,2),end=(6,2),len=6,orientation='v'}, "LONDON")
-               , (Blank {start=(4,6),end=(10,6),len=7,orientation='v'}, "ICELAND")
+    expected = [ (Blank {start=(4,2),end=(4,6),len=5,orientation=H}, "DELHI")
+               , (Blank {start=(8,3),end=(8,8),len=6,orientation=H}, "ANKARA")
+               , (Blank {start=(1,2),end=(6,2),len=6,orientation=V}, "LONDON")
+               , (Blank {start=(4,6),end=(10,6),len=7,orientation=V}, "ICELAND")
                ]
-    input = ([ Blank {start=(4,2),end=(4,6),len=5,orientation='h'}
-             , Blank {start=(8,3),end=(8,8),len=6,orientation='h'}
-             , Blank {start=(1,2),end=(6,2),len=6,orientation='v'}
-             , Blank {start=(4,6),end=(10,6),len=7,orientation='v'}
+    input = ([ Blank {start=(4,2),end=(4,6),len=5,orientation=H}
+             , Blank {start=(8,3),end=(8,8),len=6,orientation=H}
+             , Blank {start=(1,2),end=(6,2),len=6,orientation=V}
+             , Blank {start=(4,6),end=(10,6),len=7,orientation=V}
              ],
              ["LONDON","DELHI","ICELAND","ANKARA"])
 
----- printOutput
+---- showCrosswords
 
-printOutput :: [(Blank, String)] -> String
-printOutput = undefined
+showCrosswords :: [FilledBlank] -> String
+showCrosswords xs =
+  let (horizontals, verticals) = partition (\(b, _) -> orientation b==H) xs
+  in
+    (intercalate "\n" . showHorizontalWords horizontals . showVerticalWords) verticals
 
-testPrintOutput = TestCase (assertEqual "printOutput" expected (printOutput input))
+showHorizontalWords :: [FilledBlank] -> [String] -> [String]
+showHorizontalWords horizontals lines =
+  let horizontalsByRow :: Map.Map Int [FilledBlank]
+      horizontalsByRow = Map.fromListWith (++) (map (\p@(b, _) -> (fst (start b), [p])) horizontals)
+  in map (\i -> showOneLine (lines !! (i-1)) (Map.lookup i horizontalsByRow)) [1..dimension]
+
+showVerticalWords :: [FilledBlank] -> [String]
+showVerticalWords verticals =
+  let verticalsByColumn :: Map.Map Int [FilledBlank]
+      verticalsByColumn = Map.fromListWith (++) (map (\p@(b, _) -> (snd (start b), [p])) verticals)
+  in transpose (map (\i -> showOneLine (replicate dimension '+') (Map.lookup i verticalsByColumn)) [1..dimension])
+
+showOneLine :: String -> Maybe [FilledBlank] -> String
+showOneLine background fBlanksMaybe =
+  case fBlanksMaybe of
+    Nothing       -> background
+    Just fblanks  -> foldl showWord background fblanks
+
+showWord :: String -> FilledBlank -> String
+showWord background (Blank {start=(x,y),end=_,len=l,orientation=o}, word) =
+  let onset = if o == H then y else x
+      prefix = take (onset-1) background
+      suffix = drop (onset-1+l) background
+  in intercalate "" [prefix, word, suffix]
+
+testShowOneLineHorizontal  = TestCase (assertEqual "showOneLine horizontal" expected (showOneLine (replicate dimension '+') input))
+  where
+    expected = "++GET+UP++"
+    input = Just
+      [ (Blank {start=(2,3),end=(2,5),len=3,orientation=H}, "GET")
+      , (Blank {start=(2,7),end=(2,8),len=2,orientation=H}, "UP")
+      ]
+
+testShowOneLineVertical = TestCase (assertEqual "showOneLine vertical" expected (showOneLine (replicate dimension '+') input))
+  where
+    expected = "++GET+UP++"
+    input = Just
+      [ (Blank {start=(3,2),end=(5,2),len=3,orientation=V}, "GET")
+      , (Blank {start=(7,2),end=(8,2),len=2,orientation=V}, "UP")
+      ]
+
+testShowCrosswords = TestCase (assertEqual "showCrosswords" expected (showCrosswords input))
   where
     expected = intercalate "\n" [ "+L++++++++"
                                 , "+O++++++++"
@@ -174,8 +222,9 @@ testPrintOutput = TestCase (assertEqual "printOutput" expected (printOutput inpu
                                 , "+++++N++++"
                                 , "+++++D++++"
                                 ]
-    input = [ (Blank {start=(4,2),end=(4,6),len=5,orientation='h'}, "DELHI")
-            , (Blank {start=(8,3),end=(8,8),len=6,orientation='h'}, "ANKARA")
-            , (Blank {start=(1,2),end=(6,2),len=6,orientation='v'}, "LONDON")
-            , (Blank {start=(4,6),end=(10,6),len=7,orientation='v'}, "ICELAND")
+    input = [ (Blank {start=(4,2),end=(4,6),len=5,orientation=H}, "DELHI")
+            , (Blank {start=(8,3),end=(8,8),len=6,orientation=H}, "ANKARA")
+            , (Blank {start=(1,2),end=(6,2),len=6,orientation=V}, "LONDON")
+            , (Blank {start=(4,6),end=(10,6),len=7,orientation=V}, "ICELAND")
             ]
+
