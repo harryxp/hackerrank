e881942a9555d4d012381b962f662b58a52915ee
Harry Pan <pan.xingzhi@gmail.com>
2017-05-05 13:19:47 -0700

    Just have to use scanl.

--- fp/introduction/functions-or-not.hs
+++ fp/introduction/functions-or-not.hs
@@ -1,31 +1,33 @@
-import Data.List (findIndex,sort)
+import Data.List (findIndex)
 import qualified Data.Map.Lazy as Map
 
-isFunction :: [(Int,Int)] -> Bool
-isFunction xs = isFunction' Map.empty xs
-
-isFunction' m [] = True
-isFunction' m ((k,v):xs) = case Map.lookup k m of
-  Nothing -> isFunction' (Map.insert k v m) xs
-  Just v' | v == v' -> isFunction' m xs
-          | otherwise -> False
-
 main :: IO [()]
-main = getContents >>= mapM putStrLn . reverse . map (\b -> if b then "YES" else "NO") . runTestCases . lines
+main = getContents >>= mapM putStrLn . map (\b -> if b then "YES" else "NO") . runTestCases . lines
 
 runTestCases :: [String] -> [Bool]
-runTestCases (line:lines) =
+runTestCases (_:lines) =
   let testCases :: [[(Int,Int)]]
-      testCases = foldl parseLine [] lines
+      testCases = (reverse . foldl parseLine []) lines
   in
     map isFunction testCases
-runTestCases _ = error "This cannot happen."
+runTestCases [] = error "This cannot happen."
 
 parseLine :: [[(Int,Int)]] -> String -> [[(Int,Int)]]
 parseLine (x:xs) s = case findIndex (==' ') s of
   Nothing -> []:x:xs
   Just n -> let (h,t) = splitAt n s in ((read h,read t):x):xs
 parseLine [] _ = [[]]
+
+isFunction :: [(Int,Int)] -> Bool
+isFunction xs = all snd $ scanl
+  (
+    \(m,isLegal) (k,v) -> case Map.lookup k m of
+      Nothing -> ((Map.insert k v m),True)
+      Just v' | v == v' -> (m,True)
+              | otherwise -> (m,False)
+  )
+  (Map.empty,True)
+  xs
 
 {--
 Sample Input:
